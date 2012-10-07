fa28730a4b028f08a11c3e8a91ac824db0b9fa6e
Joe Carroll <joe.carroll@gmail.com>
2012-10-07 13:43:56 +0300

    Part 3 initial solution. Part 2 B still in progress.

--- part2.rb
+++ part2.rb
@@ -4,31 +4,38 @@ class NoSuchStrategyError < StandardError ; end
 def rps_result(m1, m2)
   rules = {"R" => "S", "S" => "P", "P" => "R"}
 
-  if rules[m2] == m1
-    1
+  if rules[m2] == m1  # check what 2nd player's strategy beats, and if that's what 1st player used then
+    1   # ...2nd player wins
   else
-    0
+    0   # 1st player has winning strategy or wins by default when strategies are the same
   end
 end
 
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
   raise NoSuchStrategyError unless game[0][1] =~ /R|P|S/ and game[1][1] =~ /R|P|S/
-
-  rules = {"R" => "S", "S" => "P", "P" => "R"}
-
-  game[rps_result(game[0][1], game[1][1])]
+  game[rps_result(game[0][1], game[1][1])]  # return name & strategy of winner
 end
 
 def rps_tournament_winner(tournament)
   begin
-    rps_game_winner tournament
-  rescue WrongNumberOfPlayersError
-    rps_tournament_winner tournament[0]
-    rps_tournament_winner tournament[1]
+    rps_game_winner [tournament[0],tournament[1]]
   rescue NoSuchStrategyError
-
+    tournament[0] = rps_tournament_winner tournament[0]
+    tournament[1] = rps_tournament_winner tournament[1]
+  rescue WrongNumberOfPlayersError
   end
-
-
 end
+
+#test_tourn = [
+#    [
+#        [ ["Armando", "P"], ["Dave", "S"] ],
+#        [ ["Richard", "R"],  ["Michael", "S"] ],
+#    ],
+#    [
+#        [ ["Allen", "S"], ["Omer", "P"] ],
+#        [ ["David E.", "R"], ["Richard X.", "P"] ]
+#    ]
+#]
+#
+#rps_tournament_winner test_tourn
---
+++ part3.rb
@@ -0,0 +1,19 @@
+def combine_anagrams(words)
+  anagrams = {}
+  words.each_with_index do |current_word, index|          # identify anagrams and compile their indexes into a hash
+    sorted = current_word.downcase.split(//).sort.join
+    if anagrams.key? sorted
+      anagrams[sorted].push index   # if anagram already exists, add other matches to its array
+    else
+      anagrams[sorted] = [index]    # create a new array if no anagrams for it have yet been found
+    end
+  end
+  output = []                     # initialise output array
+  anagram_group_index = 0
+  anagrams.each_key do |key|
+    output.push []          # create a new sub-array for each anagram group & collect all anagram matches into it
+    anagrams[key].each { |index| output[anagram_group_index].push words[index] }
+    anagram_group_index += 1
+  end
+  output
+end
