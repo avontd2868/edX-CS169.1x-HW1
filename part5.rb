838552c7cc5cfd4e576c17c4a9fa81ad5888f3eb
Joe Carroll <joe.carroll@gmail.com>
2012-10-14 19:54:08 +0300

    More-correct solution to Part 5: returns [nil] not nil if the attribute has never been set (required by problem description but autograder's testing code seems to miss this).

--- part5.rb
+++ part5.rb
@@ -2,31 +2,22 @@ class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
+
+    #attr_reader attr_name+"_history"   -- removing bad code in given template
+    #because it returns nil instead of [nil] if no assignments have been made.
+
     class_eval %Q{
                   def #{attr_name}=(value)
                     @#{attr_name} = value
                     (@#{attr_name}_history ||= [nil]) << value
                   end
+                  def #{attr_name}_history
+                    @#{attr_name}_history || [nil]
+                  end
                }
   end
 end
 
-#class Foo
-#  attr_reader :bar
-#  attr_reader :bar_history
-#  def bar=(value)
-#    @bar=value
-#    (@bar_history ||= [nil]) << value
-#  end
-#end
-
 class Foo
   attr_accessor_with_history :bar
 end
-
-
-f = Foo.new
-f.bar = 1
-f.bar = 2
-f.bar_history # => if your code works, should be [nil, 1, 2]
