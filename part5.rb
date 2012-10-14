dd7bafe52f7fc6c28ddd13884b1044a7beef2358
Joe Carroll <joe.carroll@gmail.com>
2012-10-14 19:11:20 +0300

    Last problem solved: attr_accessor_with_history successfully implemented with class_eval

--- part5.rb
+++ part5.rb
@@ -1,16 +1,32 @@
 class Class
   def attr_accessor_with_history(attr_name)
-    attr_name = attr_name.to_s   # make sure it's a string
-    attr_reader attr_name        # create the attribute's getter
+    attr_name = attr_name.to_s # make sure it's a string
+    attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-                  def #{attr_name}_history
-
+                  def #{attr_name}=(value)
+                    @#{attr_name} = value
+                    (@#{attr_name}_history ||= [nil]) << value
+                  end
                }
   end
 end
+
+#class Foo
+#  attr_reader :bar
+#  attr_reader :bar_history
+#  def bar=(value)
+#    @bar=value
+#    (@bar_history ||= [nil]) << value
+#  end
+#end
 
 class Foo
   attr_accessor_with_history :bar
 end
 
+
+f = Foo.new
+f.bar = 1
+f.bar = 2
+f.bar_history # => if your code works, should be [nil, 1, 2]
